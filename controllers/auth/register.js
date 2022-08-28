4cdb7a657ef885f0e3118e890b73a2eb4c7c5f69
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2022-08-28 16:19:36 +0300

    initial commit hw6

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,6 +2,8 @@ const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
 const gravatar = require("gravatar");
+const shortid = require("shortid");
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -9,14 +11,24 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
-
+  const verificationToken = shortid();
   const avatarURL = gravatar.url(email);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   const result = await User.create({
     email,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: "Подтверждение email",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Подтвердить email</a>`,
+  };
+
+  await sendEmail(mail);
+
   const { subscription } = result;
   res.status(201).json({
     status: "success",
